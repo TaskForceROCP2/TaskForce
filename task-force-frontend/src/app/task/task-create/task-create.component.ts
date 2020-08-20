e0c9247e1f3387064b26fa1e48dff945c678967e
David Galimi <djgalimi@yahoo.com>
2020-08-20 18:10:29 -0400

    fixed jenkins build errors and started create task

--- task-force-frontend/src/app/task/task-create/task-create.component.ts
+++ task-force-frontend/src/app/task/task-create/task-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-task-create',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskCreateComponent implements OnInit {
 
+  title = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateTitle() {
+    
   }
 
 }
