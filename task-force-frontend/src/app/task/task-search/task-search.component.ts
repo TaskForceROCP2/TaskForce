45d0fedf5a2b195973e28fec2d2ee0bbe16a26ee
Lisa Lampen <monalisa408@yahoo.com>
2020-08-22 03:24:57 -0400

    header & navbar search

--- task-force-frontend/src/app/task/task-search/task-search.component.ts
+++ task-force-frontend/src/app/task/task-search/task-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Task } from '../../task';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-search',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-search.component.css']
 })
 export class TaskSearchComponent implements OnInit {
+  task: Task;
+  tasks: Task[] = [];
+  searchVal = '';
+  results: Task[] =[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { 
+  
+  }
 
   ngOnInit(): void {
   }
 
+  searchTask() {
+    
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+    let x = 0;
+    for (let task in this.tasks) {
+      if (this.task.title === this.searchVal) {
+        this.results[x++] = this.task;
+      }
+    };
+    if (x === 0) { 
+      alert('Task not found');
+      this.searchVal = '';
+    }
+    else {
+      // go to card or task page list?
+    }
+  }
 }
